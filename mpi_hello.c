401cbb5d15f5e24eda384b954b57d7fa34e12eee
Kidhar Bachan <kidhar.bachan@multichoice.co.za>
2019-06-15 09:02:38 +0200

    MPI user defined datatype is working

--- mpi_hello.c
+++ mpi_hello.c
@@ -1,4 +1,4 @@
-/*#include <mpi.h>*/
+#include <mpi.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -7,14 +7,52 @@
 // Assumed scale: 100 pixels = 1AU.
 #define AU  (149.6e6 * 1000)     // 149.6 million km, in meters.
 #define SCALE  (250 / AU)
+#define NUM_ROCKETS 1600
+#define NUM_TYPES 6
+
+typedef struct {
+  char name[20];
+  double mass;
+  double px;
+  double py;
+  double vx;
+  double vy;
+}Body;
 
 int main(int argc, char** argv) {
     // Initialize the MPI environment
     int message = 10;
+#define MPI
 #ifdef MPI
     MPI_Status status;
+    Body planets[8];
+    MPI_Datatype planettype;
+    MPI_Datatype type[NUM_TYPES] = { MPI_CHAR, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE };
+    int blocklen[NUM_TYPES] = { 20, 1, 1, 1, 1, 1 };
+    MPI_Aint array_of_displacements[NUM_TYPES];
+    MPI_Aint name_addr, mass_addr, px_addr, py_addr, vx_addr, vy_addr;
 
+    /* Init MPI */
     MPI_Init(NULL, NULL);
+
+    /* Create our user defined data structure for the planets to make communication easier, later */
+    MPI_Get_address(&planets[0].name, &name_addr);
+    MPI_Get_address(&planets[0].mass, &mass_addr);
+    MPI_Get_address(&planets[0].px, &px_addr);
+    MPI_Get_address(&planets[0].py, &py_addr);
+    MPI_Get_address(&planets[0].vx, &vx_addr);
+    MPI_Get_address(&planets[0].vy, &vy_addr);
+
+    array_of_displacements[0] = 0;
+    array_of_displacements[1] = mass_addr - name_addr;
+    array_of_displacements[2] = px_addr - name_addr;
+    array_of_displacements[3] = py_addr - name_addr;
+    array_of_displacements[4] = vx_addr - name_addr;
+    array_of_displacements[5] = vy_addr - name_addr;
+
+    MPI_Type_create_struct(NUM_TYPES, blocklen, array_of_displacements, type, &planettype);
+    MPI_Type_commit(&planettype);
+
 
     // Get the number of processes
     int world_size;
@@ -31,38 +69,42 @@ int main(int argc, char** argv) {
 
     if (world_rank == 0)
     {
-      while (message)
+      /*while (message)*/
       {
-        MPI_Bcast(&message, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        printf("Sent message %d to everyone\n", message);
-        message--;
+        sprintf(planets[0].name, "%s", "earth");
+        planets[0].mass = 5.9742 * pow(10,24);
+        planets[0].px = -1*AU;
+        planets[0].py = 0;
+        planets[0].vx = 0;
+        planets[0].vy = 29.783*1000;            // 29.783 km/sec
+        MPI_Bcast(&planets[0], 1, planettype, 0, MPI_COMM_WORLD);
+        printf("Sent planet info for '%s' to everyone\n", planets[0].name);
+        /*message--;*/
       }
     }
     else
     {
-      while (message != 1) 
+      /*while (message != 1) */
       {
-        MPI_Bcast(&message, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        printf("received message %d from Process 0\n",message);
+        MPI_Bcast(&planets[0], 1, planettype, 0, MPI_COMM_WORLD);
+        printf("received planet name %s from Process 0\n",planets[0].name);
+        printf("received planet mass %e from Process 0\n",planets[0].mass);
+        printf("received planet px %e from Process 0\n",planets[0].px);
+        printf("received planet py %e from Process 0\n",planets[0].py);
+        printf("received planet vx %e from Process 0\n",planets[0].vx);
+        printf("received planet vy %e from Process 0\n",planets[0].vy);
       }
     }
     printf("Process %d terminated\n",world_rank);
     // Finalize the MPI environment.
+    MPI_Type_free(&planettype);
     MPI_Finalize();
 #else
-    #define NUM_ROCKETS 1600
-    typedef struct {
-      char name[20];
-      double mass;
-      double px;
-      double py;
-      double vx;
-      double vy;
-    }Body;
 
     Body rocket, sun, venus;
     Body *bodies;
     Body *d_bodies;
+
 
     bodies = (Body *)malloc(NUM_ROCKETS * sizeof(Body));
 
@@ -71,7 +113,7 @@ int main(int argc, char** argv) {
     double vx_arr[NUM_ROCKETS];
     double vy_arr[NUM_ROCKETS];
 
-    vr = 46;
+    vr = 46 * 1000; // 46 km/sec
 
     for (int i = 0; i < NUM_ROCKETS; i+=4)
     {
